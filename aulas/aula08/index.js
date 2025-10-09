b7215984e0385dc85c5300dd5f1ace649dab719e
Pedro <pcastrodev@gmail.com>
2025-10-09 10:41:04 -0300

    código aula 08

--- aulas/aula08/index.js
+++ aulas/aula08/index.js
@@ -1,7 +1,39 @@
 const readline = require("readline-sync");
 const conectar = require("./database");
 
-function main() {
+let db;
+let collection;
+
+async function inserir(nomeTarefa) {
+    const resultado = await collection.insertOne({ 
+        nome: nomeTarefa, 
+        concluida: false
+    });
+    console.log("Tarefa criada com sucesso!", resultado);
+}
+
+async function buscar(nomeTarefa) {
+    const resultado = await collection.find({ nome: nomeTarefa });
+    console.log("Tarefas encontradas:", resultado);
+}
+
+async function alterar(nomeTarefa, nomeAtual, concluidaAtual) {
+    const resultado = await collection.updateOne(
+        { nome: nomeTarefa },
+        { $set: { nome: nomeAtual, concluida: concluidaAtual } }
+    );
+    console.log("Tarefa alterada com sucesso!", resultado);
+}
+
+async function remover(nomeTarefa) {
+    const resultado = await collection.deleteOne({ nome: nomeTarefa });
+    console.log("Tarefa removida com sucesso!", resultado);
+}
+
+async function main() {
+    db = await conectar();
+    collection = db.collection("tarefas");
+
     while (true) {
         console.log("MENU PRINCIPAL");
         console.log("1 - Criar tarefa");
@@ -13,24 +45,34 @@ function main() {
         const opcao = readline.question("Escolha uma opção: ");
 
         switch (opcao) {
-            case "1":
-                criarTarefa();
+            case "1": {
+                const nome = readline.question("Nome da tarefa: ");
+                await inserir(nome);
                 break;
-            case "2":
-                buscarTarefa();
+            }
+            case "2": {
+                const nome = readline.question("Informe o nome da tarefa: ");
+                await buscar(nome);
                 break;
-            case "3":
-                alterarTarefa();
+            }
+            case "3": {
+                const nome = readline.question("Informe o nome da tarefa a ser alterada: ");
+                const nomeAtual = readline.question("Informe o novo nome da tarefa: ");
+                const concluidaAtual = readline.question("Informe a situação da tarefa: ");
+                await alterar(nome, nomeAtual, concluidaAtual);
                 break;
-            case "4":
-                removerTarefa();
+            }
+            case "4": {
+                const nome = readline.question("Informe o nome da tarefa a ser removida: ");
+                await remover(nome);
                 break;
+            }
             case "5":
-                console.log("Saindo...");
                 process.exit(0);
             default:
                 console.log("Opção inválida!");
-                main();
         }
     }
 }
+
+main();
