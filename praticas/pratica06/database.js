f78234328b9e5cab74290164895d44c87d1aa09b
Pedro Henrique Almeida Castro <pcastrodev@gmail.com>
2025-10-14 23:41:48 -0300

    removendo a senha

--- praticas/pratica06/database.js
+++ praticas/pratica06/database.js
@@ -1,5 +1,5 @@
 const { MongoClient } = require('mongodb')
-const url = process.env.MONGODB_URI || 'mongodb+srv://pedro_poderoso:admin123@cluster0.udzcqn7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+const url = process.env.MONGODB_URI || 'mongodb+srv://@cluster0.udzcqn7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 const client = new MongoClient(url)
 async function conectarDb() {
   if (!client.topology || !client.topology.isConnected()) {
